4751f998faa6f7424fe75ab9009f1bcfcd223484
Ivan Kononovich <fogfug1@gmail.com>
2019-07-10 22:39:09 +0300

    feat: add header

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//--------------------------------------------------
+// start burger script
+//--------------------------------------------------
+
+const burger = document.querySelector('.burger');
+
+function changeStateBurger() {
+    const burgerLines = document.querySelector('.burger__container-lines');
+    const burgerContainerContent = document.querySelector('.burger__container');
+    const header = document.querySelector('.header');
+
+    burgerLines.classList.toggle('burger__container-lines_open');
+    burgerContainerContent.classList.toggle('burger__container_open');
+    header.classList.toggle('header_blocking');
+}
+
+burger.addEventListener('click', changeStateBurger);
+burger.addEventListener('touch', changeStateBurger);
+
+//--------------------------------------------------
+// end burger script
+//--------------------------------------------------
